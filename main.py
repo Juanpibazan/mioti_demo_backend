8c267a9bfe23d4fee4f58875af7fba539ba9f5ba
Juanpibazan <juampivader1@gmail.com>
2024-06-02 00:03:14 -0400

    changed pickle file

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import pickle
 from fastapi import FastAPI
+from pydantic import BaseModel
+import sklearn
+
+rf_model = pickle.load(open('rf2_model.pkl','rb'))
 
 app = FastAPI()
 
 @app.get('/')
 async def root():
     return {'message':'Bienvenido a Movie Advisor!!'}
+
+
+class Characteristics(BaseModel):
+        title: str
+        startYear: int
+        genre: str
+        minutes: int
+        personType: str
+        personName: str
+        personAge: float
+
+@app.post('/')
+async def predict(characteristics: Characteristics):
+    
+    return characteristics
